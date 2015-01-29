8aa6be6119e78c5ecb7153ebb45d31d0b8aab505
Phil Murray <pmurray@riverainc.com>
2015-01-29 06:31:39 -0500

    added constraints on word related tables and updated usf import script to be fault tolerant

--- utils/associations.sql
+++ utils/associations.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.5
 -- Dumped by pg_dump version 9.3.5
--- Started on 2015-01-26 16:53:47 EST
+-- Started on 2015-01-29 06:29:54 EST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 2026 (class 1262 OID 16385)
+-- TOC entry 2040 (class 1262 OID 16385)
 -- Name: associations; Type: DATABASE; Schema: -; Owner: associations_dbuser
 --
 
@@ -33,7 +33,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 177 (class 3079 OID 11787)
+-- TOC entry 178 (class 3079 OID 11787)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -41,8 +41,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2029 (class 0 OID 0)
--- Dependencies: 177
+-- TOC entry 2043 (class 0 OID 0)
+-- Dependencies: 178
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -50,7 +50,7 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
--- TOC entry 178 (class 3079 OID 16387)
+-- TOC entry 179 (class 3079 OID 16387)
 -- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -58,8 +58,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 
 --
--- TOC entry 2030 (class 0 OID 0)
--- Dependencies: 178
+-- TOC entry 2044 (class 0 OID 0)
+-- Dependencies: 179
 -- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -174,7 +174,19 @@ CREATE TABLE users (
 ALTER TABLE public.users OWNER TO associations_dbuser;
 
 --
--- TOC entry 1908 (class 2606 OID 16430)
+-- TOC entry 177 (class 1259 OID 16468)
+-- Name: words; Type: TABLE; Schema: public; Owner: associations_dbuser; Tablespace: 
+--
+
+CREATE TABLE words (
+    text text NOT NULL
+);
+
+
+ALTER TABLE public.words OWNER TO associations_dbuser;
+
+--
+-- TOC entry 1914 (class 2606 OID 16430)
 -- Name: games_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
 --
 
@@ -183,7 +195,7 @@ ALTER TABLE ONLY games
 
 
 --
--- TOC entry 1910 (class 2606 OID 16439)
+-- TOC entry 1918 (class 2606 OID 16439)
 -- Name: picks_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
 --
 
@@ -192,7 +204,7 @@ ALTER TABLE ONLY picks
 
 
 --
--- TOC entry 1904 (class 2606 OID 16414)
+-- TOC entry 1909 (class 2606 OID 16414)
 -- Name: session_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
 --
 
@@ -201,7 +213,7 @@ ALTER TABLE ONLY session
 
 
 --
--- TOC entry 1902 (class 2606 OID 16406)
+-- TOC entry 1907 (class 2606 OID 16406)
 -- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
 --
 
@@ -210,7 +222,7 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 1906 (class 2606 OID 16424)
+-- TOC entry 1912 (class 2606 OID 16424)
 -- Name: usf_norms_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
 --
 
@@ -219,7 +231,49 @@ ALTER TABLE ONLY usf_norms
 
 
 --
--- TOC entry 1911 (class 2606 OID 16440)
+-- TOC entry 1920 (class 2606 OID 16475)
+-- Name: words_pkey; Type: CONSTRAINT; Schema: public; Owner: associations_dbuser; Tablespace: 
+--
+
+ALTER TABLE ONLY words
+    ADD CONSTRAINT words_pkey PRIMARY KEY (text);
+
+
+--
+-- TOC entry 1915 (class 1259 OID 16481)
+-- Name: cov; Type: INDEX; Schema: public; Owner: associations_dbuser; Tablespace: 
+--
+
+CREATE INDEX cov ON picks USING btree ("from");
+
+
+--
+-- TOC entry 1916 (class 1259 OID 16487)
+-- Name: cov2; Type: INDEX; Schema: public; Owner: associations_dbuser; Tablespace: 
+--
+
+CREATE INDEX cov2 ON picks USING btree ("to");
+
+
+--
+-- TOC entry 1910 (class 1259 OID 16503)
+-- Name: cov3; Type: INDEX; Schema: public; Owner: associations_dbuser; Tablespace: 
+--
+
+CREATE INDEX cov3 ON usf_norms USING btree ("to");
+
+
+--
+-- TOC entry 1925 (class 2606 OID 16476)
+-- Name: picks_from_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
+--
+
+ALTER TABLE ONLY picks
+    ADD CONSTRAINT picks_from_fkey FOREIGN KEY ("from") REFERENCES words(text);
+
+
+--
+-- TOC entry 1923 (class 2606 OID 16440)
 -- Name: picks_gameId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
 --
 
@@ -228,7 +282,16 @@ ALTER TABLE ONLY picks
 
 
 --
--- TOC entry 1912 (class 2606 OID 16445)
+-- TOC entry 1926 (class 2606 OID 16482)
+-- Name: picks_to_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
+--
+
+ALTER TABLE ONLY picks
+    ADD CONSTRAINT picks_to_fkey FOREIGN KEY ("to") REFERENCES words(text);
+
+
+--
+-- TOC entry 1924 (class 2606 OID 16445)
 -- Name: picks_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
 --
 
@@ -237,7 +300,25 @@ ALTER TABLE ONLY picks
 
 
 --
--- TOC entry 2028 (class 0 OID 0)
+-- TOC entry 1921 (class 2606 OID 16488)
+-- Name: usf_norms_from_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
+--
+
+ALTER TABLE ONLY usf_norms
+    ADD CONSTRAINT usf_norms_from_fkey FOREIGN KEY ("from") REFERENCES words(text);
+
+
+--
+-- TOC entry 1922 (class 2606 OID 16498)
+-- Name: usf_norms_to_fkey; Type: FK CONSTRAINT; Schema: public; Owner: associations_dbuser
+--
+
+ALTER TABLE ONLY usf_norms
+    ADD CONSTRAINT usf_norms_to_fkey FOREIGN KEY ("to") REFERENCES words(text);
+
+
+--
+-- TOC entry 2042 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -249,7 +330,7 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 GRANT ALL ON SCHEMA public TO associations_dbuser;
 
 
--- Completed on 2015-01-26 16:53:48 EST
+-- Completed on 2015-01-29 06:29:54 EST
 
 --
 -- PostgreSQL database dump complete
