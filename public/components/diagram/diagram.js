3df7af1c77eb733e57c3e76722ff82d5f92247ba
Phil Murray <pmurray@riverainc.com>
2015-01-30 07:11:19 -0500

    finished moving the diagram stuff into a reusable component

--- public/components/diagram/diagram.js
+++ public/components/diagram/diagram.js
@@ -1,7 +1,21 @@
 "use strict";
 
 angular.module('associations.components.diagram', [])
-.factory('DiagramService', [function(){
+
+.constant("plumbDefaults",{
+	Connector: "StateMachine",
+	Endpoints: [["Dot",{radius:7, cssClass:"endpoint"}],"Blank"],
+	Overlays: [["PlainArrow", {cssClass:"arrow", width: 15, length: 15, location:1}]]
+})
+
+.constant("springyDefaults",{
+	stiffness: 50.0,
+	repulsion: 10.0,
+	damping: 0.85,
+	minEnergyThreshold: 0.07
+})
+
+.factory('MyForceDirected', [function(){
 
 	var MyForceDirected = function() {
 		Springy.Layout.ForceDirected.apply(this, Array.prototype.slice.call(arguments, 0));
@@ -27,49 +41,111 @@ angular.module('associations.components.diagram', [])
 		return this.nodePoints[node.id];
 	};
 
+	return MyForceDirected;
+}])
+
+.directive("diagram", ["$window", "$timeout", "plumbDefaults", "springyDefaults", "MyForceDirected", function ($window, $timeout, plumbDefaults, springyDefaults, MyForceDirected) {
 	return {
-		getGraph: function(element, words, links, plumbInstance){
+		restrict: 'EA',
+		transclude: true,
+		replace: true,
+		scope: {
+			model:'=diagramModel',
+			config:"=diagramConfig"
+		},
+		template: '<div style="position:relative; width:100%; height:100%">' +
+			'<ng-transclude></ng-transclude>' +
+			'</div>',
+		link: function($scope, $element, attrs) {
+			var w = angular.element($window),
+			element = $element[0],
+			container = element.querySelector('#navContainer');
+
+			// Browser onresize event
+			w.bind('resize', function () {
+				$scope.$apply();
+			});
+
+			// Watch for resize event
+			$scope.$watch(function() {
+				return {
+					width: w[0].innerWidth,
+					height: w[0].innerHeight
+				};
+			}, function() {
+				$scope.render();
+			},true);
+
+			$scope.config = $scope.config || {};
+			$scope.config.jsPlumb = angular.extend(angular.copy(plumbDefaults), $scope.config.jsPlumb || {});
+			$scope.config.jsPlumb.Container = element;
+			$scope.config.springy = angular.extend(angular.copy(springyDefaults), $scope.config.springy || {});
 
 			var toScreen = function(p) {
-					var sx = element.offsetWidth/2 + (element.offsetWidth/10) * p.x;
-					var sy = element.offsetHeight/2 + (element.offsetHeight/10) * p.y;
-					return new Springy.Vector(sx, sy);
-				},
-				render = function render() {
-					layout.eachNode(function(node, point) {
-						var ele = node.data.element,
-						p = toScreen(point.p);
+				var sx = element.offsetWidth/2 + (element.offsetWidth/10) * p.x;
+				var sy = element.offsetHeight/2 + (element.offsetHeight/10) * p.y;
+				return new Springy.Vector(sx, sy);
+			};
 
-						ele.style.left = (p.x - ele.offsetWidth/2) + 'px';
-						ele.style.top = (p.y - ele.offsetHeight/2) + 'px';
-					});
-					plumbInstance && plumbInstance.repaintEverything();
-				};
+			var render = function render() {
+				$scope.layout.eachNode(function(node, point) {
+					var ele = node.data.element,
+					p = toScreen(point.p);
 
-			var graph = new Springy.Graph();
+					ele.style.left = (p.x - ele.offsetWidth/2) + 'px';
+					ele.style.top = (p.y - ele.offsetHeight/2) + 'px';
+				});
+				$scope.plumbInstance.repaintEverything();
+			};
 
-			//add nodes
-			var nodes = {};
 			var addNode = function(node){
-				nodes[node.id] = graph.newNode({
+				return $scope.graph.newNode({
 					element: element.querySelector('#' + node.id),
 					initial: node.initial ? new Springy.Vector(node.initial.x, node.initial.y) : undefined,
 					fixed: node.fixed
 				});
 			};
-			words.forEach(addNode);
 
-			links.forEach(function(connection){
-				graph.newEdge(nodes[connection.from.id], nodes[connection.to.id]);
-			});
+			var initDiagram = function(){
+				jsPlumb.ready(function(){
+					$scope.plumbInstance = $scope.plumbInstance || jsPlumb.getInstance($scope.config.jsPlumb);
 
-			var layout = new MyForceDirected(graph,50.0, 10.0, 0.85, 0.07);
+					$scope.graph = new Springy.Graph();
 
-			return {
-				layout: layout,
-				graph: graph,
-				start: layout.start.bind(layout, render)
+					//add nodes
+					var nodes = {};
+					$scope.model.nodes.forEach(function(node){
+						nodes[node.id] = addNode(node);
+					});
+
+					$scope.model.links.forEach(function(connection){
+						$scope.graph.newEdge(nodes[connection.from.id], nodes[connection.to.id]);
+						$scope.plumbInstance.connect({
+							source:connection.from.id,
+							target:connection.to.id,
+							anchor:[connection.from.anchor, connection.to.anchor]
+						});
+					});
+
+					$scope.layout = new MyForceDirected(
+						$scope.graph,
+						$scope.config.springy.stiffness,
+						$scope.config.springy.repulsion,
+						$scope.config.springy.damping,
+						$scope.config.springy.minEnergyThreshold);
+
+					$scope.layout.start(render);
+				});
 			};
-		}
-	};
-}]);
+
+			$scope.render = function() {
+				if (!$scope.layout){
+					$timeout(initDiagram);
+				} else {
+					$scope.layout.start();
+				}
+			};
+
+
+		}};
+	}]);
--- public/pages/main/main.js
+++ public/pages/main/main.js
@@ -1,155 +1,180 @@
 "use strict";
 
-angular.module('associations.pages.main',["associations.pages.main.components.nav"])
+angular.module('associations.pages.main', ["associations.components.diagram"])
 
-.controller("MainController", ["$scope", function ($scope) {
-	$scope.nav = {
-		words: [
-			{
-				id:'Play',
-				text:"Play",
-				initial:{x:0,y:-0.5}
-			},
-			{
-				id:'Explore',
-				text:"Explore",
-				initial:{x:-0.5,y:0.5}
-			},
-			{
-				id:'Me',
-				text:"Me",
-				initial:{x:0.5,y:0.5}
+.controller("MainController", ["$scope", function($scope) {
+	$scope.config = {};
+	$scope.model = {
+		nodes: [{
+			word:true,
+			id: 'Play',
+			text: "Play",
+			initial: {
+				x: 0,
+				y: -0.5
 			}
-		],
-		links:[{
+		}, {
+			word:true,
+			id: 'Explore',
+			text: "Explore",
+			initial: {
+				x: -0.5,
+				y: 0.5
+			}
+		}, {
+			word:true,
+			id: 'Me',
+			text: "Me",
+			initial: {
+				x: 0.5,
+				y: 0.5
+			}
+		}, {
+			link:true,
 			id: "playerProfile",
 			href: "#/playerProfile",
 			text: "Profile",
-			initial:{x:1,y:1},
+			initial: {
+				x: 1,
+				y: 1
+			},
 			anchor: ""
-		},{
+		}, {
+			link:true,
 			id: "playerStatistics",
 			href: "#/playerStatistics",
 			text: "Statistics",
-			initial:{x:1,y:0.75}
-		},{
+			initial: {
+				x: 1,
+				y: 0.75
+			}
+		}, {
+			link:true,
 			id: "exploreWords",
 			href: "#/exploreWords",
 			text: "Words",
-			initial:{x:-1,y:1}
-		},{
+			initial: {
+				x: -1,
+				y: 1
+			}
+		}, {
+			link:true,
 			id: "exploreStatistics",
 			href: "#/globalStatistics",
 			text: "Statistics",
-			initial:{x:-1,y:0.75}
-		},{
+			initial: {
+				x: -1,
+				y: 0.75
+			}
+		}, {
+			link:true,
 			id: "singleGame",
 			href: "#/singleGame",
 			text: "Single",
-			initial:{x:-0.5,y:-0.5}
-		},{
+			initial: {
+				x: -0.5,
+				y: -0.5
+			}
+		}, {
+			link:true,
 			id: "multiGame",
 			href: "#/multiGame",
 			text: "Multiplayer",
-			initial:{x:0.5,y:-0.5}
-		}],
-		title: {
-			id:"Associations",
-			text:"Associations",
-			initial:{x:0,y:0},
-			fixed:true
-		},
-		connections: [
-			{
-				from: {
-					id:"Associations",
-					anchor:"Top"
-				},
-				to: {
-					id:"Play",
-					anchor:"Bottom"
-				}
-			},
-			{
-				from: {
-					id:"Associations",
-					anchor:"Left"
-				},
-				to: {
-					id:"Explore",
-					anchor:"Right"
-				}
-			},
-			{
-				from: {
-					id:"Associations",
-					anchor:"Right"
-				},
-				to: {
-					id:"Me",
-					anchor:"Left"
-				}
-			},
-			{
-				from: {
-					id:"Explore",
-					anchor:"Bottom"
-				},
-				to: {
-					id:"exploreWords",
-					anchor:"Top"
-				}
-			},
-			{
-				from: {
-					id:"Explore",
-					anchor:"Left"
-				},
-				to: {
-					id:"exploreStatistics",
-					anchor:"Right"
-				}
-			},
-			{
-				from: {
-					id:"Me",
-					anchor:"Bottom"
-				},
-				to: {
-					id:"playerProfile",
-					anchor:"Top"
-				}
-			},
-			{
-				from: {
-					id:"Me",
-					anchor:"Right"
-				},
-				to: {
-					id:"playerStatistics",
-					anchor:"Left"
-				}
-			},
-			{
-				from: {
-					id:"Play",
-					anchor:"Top"
-				},
-				to: {
-					id:"singleGame",
-					anchor:"Bottom"
-				}
-			},
-			{
-				from: {
-					id:"Play",
-					anchor:"Top"
-				},
-				to: {
-					id:"multiGame",
-					anchor:"Bottom"
-				}
+			initial: {
+				x: 0.5,
+				y: -0.5
 			}
-		]
+		}, {
+			title:true,
+			id: "Associations",
+			text: "Associations",
+			initial: {
+				x: 0,
+				y: 0
+			},
+			fixed: true
+		}],
+		links: [{
+			from: {
+				id: "Associations",
+				anchor: "Top"
+			},
+			to: {
+				id: "Play",
+				anchor: "Bottom"
+			}
+		}, {
+			from: {
+				id: "Associations",
+				anchor: "Left"
+			},
+			to: {
+				id: "Explore",
+				anchor: "Right"
+			}
+		}, {
+			from: {
+				id: "Associations",
+				anchor: "Right"
+			},
+			to: {
+				id: "Me",
+				anchor: "Left"
+			}
+		}, {
+			from: {
+				id: "Explore",
+				anchor: "Bottom"
+			},
+			to: {
+				id: "exploreWords",
+				anchor: "Top"
+			}
+		}, {
+			from: {
+				id: "Explore",
+				anchor: "Left"
+			},
+			to: {
+				id: "exploreStatistics",
+				anchor: "Right"
+			}
+		}, {
+			from: {
+				id: "Me",
+				anchor: "Bottom"
+			},
+			to: {
+				id: "playerProfile",
+				anchor: "Top"
+			}
+		}, {
+			from: {
+				id: "Me",
+				anchor: "Right"
+			},
+			to: {
+				id: "playerStatistics",
+				anchor: "Left"
+			}
+		}, {
+			from: {
+				id: "Play",
+				anchor: "Top"
+			},
+			to: {
+				id: "singleGame",
+				anchor: "Bottom"
+			}
+		}, {
+			from: {
+				id: "Play",
+				anchor: "Top"
+			},
+			to: {
+				id: "multiGame",
+				anchor: "Bottom"
+			}
+		}]
 	};
 }]);
