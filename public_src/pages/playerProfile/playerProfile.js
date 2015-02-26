35fb0d240115c5fc80494ac3e6104e5f22bc3ec9
Phil Murray <pmurray@riverainc.com>
2015-02-26 07:03:23 -0500

    added some form validations to the profile page

--- public_src/pages/playerProfile/playerProfile.js
+++ public_src/pages/playerProfile/playerProfile.js
@@ -1,7 +1,18 @@
 "use strict";
 
-angular.module('associations.pages.playerProfile',[])
+angular.module('associations.pages.playerProfile',[
+	'directives.inputMatch',
+	'associations.components.data.user'])
 
-.controller("PlayerProfileController", ["$scope", "user", function ($scope, user) {
+.controller("PlayerProfileController", ["$scope", "user", "UserService", function ($scope, user, UserService) {
+	var blank = "................";
+
 	$scope.user = angular.extend({},user.data);
+	$scope.user.password = $scope.passwordConfirm = $scope.user.hasPassword ? blank : "";
+
+	$scope.forms = {};
+
+	$scope.save = function (){
+
+	};
 }]);
