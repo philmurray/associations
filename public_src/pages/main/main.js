7ba61892a60ec1bb11b8ce87d2d2a46885b610ee
Phil Murray <pmurray@riverainc.com>
2015-03-11 10:14:06 -0400

    more responsive tweaks

--- public_src/pages/main/main.js
+++ public_src/pages/main/main.js
@@ -8,187 +8,26 @@ angular.module('associations.pages.main', [
 
 	$scope.color = color;
 
-	$scope.words = {
-		Play: {
-			text: "Play",
-			initial: {
-				x: 0,
-				y: -0.55
-			}
+	$scope.links = [
+		{
+			href: "#/play",
+			text: "Play!",
+			description: "High intesity malarky. Single or multiplayer."
 		},
-		Explore: {
-			text: "Explore",
-			initial: {
-				x: -0.5,
-				y: 0.5
-			}
-		},
-		Me: {
-			word: true,
-			text: "Me",
-			initial: {
-				x: 0.5,
-				y: 0.5
-			}
-		}
-	};
-	$scope.links = {
-		playerProfile: {
+		{
 			href: "#/playerProfile",
 			text: "Profile",
-			initial: {
-				x: 1,
-				y: 1
-			},
-			anchor: ""
+			description: "Change your info.  See your Stats."
 		},
-		playerStatistics: {
-			href: "#/playerStatistics",
-			text: "Statistics",
-			initial: {
-				x: 1,
-				y: 0.75
-			}
-		},
-		exploreWords: {
+		{
 			href: "#/exploreWords",
 			text: "Words",
-			initial: {
-				x: -1,
-				y: 1
-			}
+			description: "oooh! pretty word graphs!  (Works best on large screens)"
 		},
-		exploreStatistics: {
+		{
 			href: "#/globalStatistics",
 			text: "Statistics",
-			initial: {
-				x: -1,
-				y: 0.75
-			}
-		},
-		singleGame: {
-			href: "#/singleGame",
-			text: "Single",
-			initial: {
-				x: -0.5,
-				y: -0.5
-			}
-		},
-		multiGame: {
-			href: "#/multiGame",
-			text: "Multiplayer",
-			initial: {
-				x: 0.5,
-				y: -0.5
-			}
+			description: "Complex analytics using the datas.  All the datas."
 		}
-	};
-
-	$scope.title = {
-		Associations: {
-			text: "Associations",
-			initial: {
-				x: 0,
-				y: 0
-			},
-			fixed: true
-		}
-	};
-
-	$scope.config = {};
-	$scope.model = {
-		nodes: {
-		},
-		links: [{
-			source: {
-				id: "Associations",
-				anchor: "Top"
-			},
-			target: {
-				id: "Play",
-				anchor: "Bottom"
-			}
-		},
-		{
-			source: {
-				id: "Associations",
-				anchor: "Left"
-			},
-			target: {
-				id: "Explore",
-				anchor: "Right"
-			}
-		},
-		{
-			source: {
-				id: "Associations",
-				anchor: "Right"
-			},
-			target: {
-				id: "Me",
-				anchor: "Left"
-			}
-		},
-		{
-			source: {
-				id: "Explore",
-				anchor: "Bottom"
-			},
-			target: {
-				id: "exploreWords",
-				anchor: "Top"
-			}
-		},
-		{
-			source: {
-				id: "Explore",
-				anchor: "Left"
-			},
-			target: {
-				id: "exploreStatistics",
-				anchor: "Right"
-			}
-		},
-		{
-			source: {
-				id: "Me",
-				anchor: "Bottom"
-			},
-			target: {
-				id: "playerProfile",
-				anchor: "Top"
-			}
-		},
-		{
-			source: {
-				id: "Me",
-				anchor: "Right"
-			},
-			target: {
-				id: "playerStatistics",
-				anchor: "Left"
-			}
-		},
-		{
-			source: {
-				id: "Play",
-				anchor: "Top"
-			},
-			target: {
-				id: "singleGame",
-				anchor: "Bottom"
-			}
-		},
-		{
-			source: {
-				id: "Play",
-				anchor: "Top"
-			},
-			target: {
-				id: "multiGame",
-				anchor: "Bottom"
-			}
-		}]
-	};
-	angular.extend($scope.model.nodes,$scope.words,$scope.links,$scope.title);
+	];
 }]);
