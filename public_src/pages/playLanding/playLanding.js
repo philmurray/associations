205d23b01283fcf15ad4f338501bfcce19ebaac0
Phil Murray <pmurray@riverainc.com>
2015-03-12 17:16:31 -0400

    added some styling to the playLanding page (plus some fake data)

--- public_src/pages/playLanding/playLanding.js
+++ public_src/pages/playLanding/playLanding.js
@@ -5,6 +5,95 @@ angular.module('associations.pages.playLanding',[])
 .controller("PlayLandingController", ["$scope", "user", "color", function ($scope, user, color) {
 	$scope.color = color;
 
+	$scope.multiGames = [
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Pending",
+			notifications: 5,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Waiting...",
+			notifications: 0,
+			players: [
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		},
+		{
+			link: "123456789",
+			time: new Date(),
+			status: "Completed",
+			notifications: 2,
+			players: [
+				{alias:"George"},
+				{alias:"Lucas"},
+				{alias:"Frank"}
+			]
+		}
+	];
+
 	$scope.startSingleGame = function(){
 
 	};
