b41c782f51bb8941f60544180c6360d7e54d93b3
Phil Murray <pmurray@riverainc.com>
2015-03-12 23:17:02 -0400

    tweaks to the game landing page.  some color changes

--- public_src/pages/playLanding/playLanding.js
+++ public_src/pages/playLanding/playLanding.js
@@ -5,25 +5,49 @@ angular.module('associations.pages.playLanding',[])
 .controller("PlayLandingController", ["$scope", "user", "color", function ($scope, user, color) {
 	$scope.color = color;
 
+	$scope.rematch = function(game, event){
+		event.cancelBubble = true;
+	};
+
+	$scope.gotoGame = function(game){
+
+	};
+
+	$scope.formatPlayerNames = function(players){
+		return players.map(function(player){return player.alias;}).join(', ');
+	};
+
+	$scope.singleGames = [
+		{
+			status: "Completed",
+			link: "123456789",
+			time: new Date()
+		}
+	];
+
 	$scope.multiGames = [
 		{
 			link: "123456789",
 			time: new Date(),
 			status: "Pending",
+			statusText: "Waiting for you",
 			notifications: 5,
+			winner: null,
 			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
+				{alias:"George", id:12345},
+				{alias:"Lucas", id:12346},
+				{alias:"Frank", id:12347}
 			]
 		},
 		{
 			link: "123456789",
 			time: new Date(),
-			status: "Waiting...",
+			status: "Waiting",
+			statusText: "Waiting for them",
 			notifications: 0,
+			winner: null,
 			players: [
-				{alias:"Frank"}
+				{alias:"Frank", id:1234}
 			]
 		},
 		{
@@ -31,10 +55,11 @@ angular.module('associations.pages.playLanding',[])
 			time: new Date(),
 			status: "Completed",
 			notifications: 2,
+			winner: 1234,
 			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
+				{alias:"George colbrecth", id:1234},
+				{alias:"Lucas do matchor", id:12345},
+				{alias:"Franky mansion", id:12346}
 			]
 		},
 		{
@@ -42,10 +67,11 @@ angular.module('associations.pages.playLanding',[])
 			time: new Date(),
 			status: "Completed",
 			notifications: 2,
+			winner: 1234,
 			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
+				{alias:"George", id:12345},
+				{alias:"Lucas", id:1234},
+				{alias:"Frank", id:12346}
 			]
 		},
 		{
@@ -53,10 +79,11 @@ angular.module('associations.pages.playLanding',[])
 			time: new Date(),
 			status: "Completed",
 			notifications: 2,
+			winner: 1234,
 			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
+				{alias:"George", id:12346},
+				{alias:"Lucas", id:1234},
+				{alias:"Frank", id:12345}
 			]
 		},
 		{
@@ -64,37 +91,12 @@ angular.module('associations.pages.playLanding',[])
 			time: new Date(),
 			status: "Completed",
 			notifications: 2,
+			winner: 1234,
 			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
-			]
-		},
-		{
-			link: "123456789",
-			time: new Date(),
-			status: "Completed",
-			notifications: 2,
-			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
-			]
-		},
-		{
-			link: "123456789",
-			time: new Date(),
-			status: "Completed",
-			notifications: 2,
-			players: [
-				{alias:"George"},
-				{alias:"Lucas"},
-				{alias:"Frank"}
+				{alias:"George", id:1234},
+				{alias:"Lucas", id:12345},
+				{alias:"Frank", id:12346}
 			]
 		}
 	];
-
-	$scope.startSingleGame = function(){
-
-	};
 }]);
