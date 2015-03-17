5e4bd0d5af6dc19474d810ec5979aed5bf3be3e0
Phil Murray <pmurray@riverainc.com>
2015-03-17 07:06:00 -0400

    fixed some color issues

--- utils/sql/color-data.sql
+++ utils/sql/color-data.sql
@@ -16,16 +16,15 @@ SET search_path = public, pg_catalog;
 --
 
 COPY colors (id, name, hex, css_class, is_default) FROM stdin;
-68927a58-0359-439b-87ea-843e2e8608da	Neon Purple	#bc13fe	neon-purple	t
-c1b07aea-5efb-4434-813c-0850c05ecf46	Neon Green	#54fd13	neon-green	f
-6db6733c-bf22-42f7-95be-790ca228304b	Neon Blue	#13fdbc	neon-blue	f
-9727530c-23c9-490e-9f38-bac5bada5658	Neon Red	#fc1353	neon-red	f
-6f548599-54f2-492b-9dc1-3328d777fe6d	Neon Orange	#fbbb12	neon-orange	f
-cee7c445-8d10-4473-ba66-d50cc2442b4d	Neon Yellow	#ccff15	neon-yellow	f
+68927a58-0359-439b-87ea-843e2e8608da	Neon Purple	#9D08FB	neon-purple	f
+c1b07aea-5efb-4434-813c-0850c05ecf46	Neon Green	#AAFE00	neon-green	t
+6db6733c-bf22-42f7-95be-790ca228304b	Neon Blue	#00FBD8	neon-blue	f
+9727530c-23c9-490e-9f38-bac5bada5658	Neon Pink	#FD008F	neon-pink	f
+6f548599-54f2-492b-9dc1-3328d777fe6d	Neon Orange	#FF6A00	neon-orange	f
+cee7c445-8d10-4473-ba66-d50cc2442b4d	Neon Yellow	#FFFC00	neon-yellow	f
 \.
 
 
 --
 -- PostgreSQL database dump complete
 --
-
